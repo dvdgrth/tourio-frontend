8b58284e3b28ac571c56b22b83c2eb7483d1d26f
dvdgrth <david.guerth@gmail.com>
2021-01-17 16:59:41 +0100

    removed logo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Banner() {
   return (
     <div className="banner">
-      <img src="logo.png" alt="logo" className="banner-logo" />
+      {/* <img src="logo.png" alt="logo" className="banner-logo" /> */}
       <h1 className="banner-title">MyLink🔗YourLink</h1>
       <Link to="/about">
         <h1>Search the web like never before</h1>
